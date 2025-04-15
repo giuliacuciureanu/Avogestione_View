e2f116bbb0ddba456cee4063fe7d41564aee2dce
JacopoDiStasio <s8145833s@studenti.itisavogadro.it>
2025-04-15 15:37:07 +0200

    Reso php

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
 <html>
 <body>
 
-<h2>Benvenuto, inserisci i tuoi dati:</h2>
+<h2>Bentornato, inserisci i tuoi dati per accedere:</h2>
 
 <form>
   <label for="username">Username:</label><br>
   <input type="text" id="username" name="unsername"><br>
   <label for="password">Password:</label><br>
-  <input type="text" id="password" name="password">
+  <input type="password" id="password" name="password">
   <input type="submit" value="Invio">
 </form>
 
