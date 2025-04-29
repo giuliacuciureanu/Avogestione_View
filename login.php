ccb36afbcad6aa05c51ff6d522fbcdfe1b0700fd
JacopoDiStasio <s8145833s@studenti.itisavogadro.it>
2025-04-29 15:29:02 +0200

    miglioramento_login

--- login.php
+++ login.php
@@ -13,9 +13,12 @@
   <label for="username">Username:</label><br>
   <input type="text" id="username" name="username"><br>
   <label for="password">Password:</label><br>
-  <input type="password" id="password" name="password">
-  <label for="ruolo">Ruolo:</label><br>
-  <input type="text" id="ruolo" name="ruolo">
+  <input type="password" id="password" name="password"><br>
+  <label for="ruolo">Ruolo</label>
+      <select name="ruolo" id="ruolo">
+        <option value="Organizzatore">Organizzatore</option>
+        <option value="Studente">Studente</option>
+      </select>
   <input type="submit" value="Invio">
 </form>
 
